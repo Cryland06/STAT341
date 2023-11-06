f6d5175e324be8fec6966055485413c01aada1e7
Cryland06 <144469567+Cryland06@users.noreply.github.com>
2023-11-06 15:35:02 -0500

    Add files via upload

---
+++ in-class-assignment-6.R
@@ -0,0 +1,10 @@
+Welch Two Sample t-test
+
+data:  x and y
+t = -72.512, df = 197.96, p-value < 2.2e-16
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -307.7685 -291.4715
+sample estimates:
+mean of x mean of y 
+    50.50    350.12 
